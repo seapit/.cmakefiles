88a870d5c9e8b54752ba244d04291e21af11a9f4
carlo pitruzzello <carlo.pitruzzello+git@gmail.com>
2024-07-14 08:15:10 +0000

    Other half of changes lol

--- list_dir.sh
+++ list_dir.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Directory to list
-directory="/workspace/cpp"
+directory="/workspaces/cpp"
 
 # List directories within the specified directory
 directories=()
--- repoaddresser.sh
+++ repoaddresser.sh
@@ -3,23 +3,24 @@
 # Source the output from find_git_root.sh
 echo "$1 $2"
 
+"/workspace/cpp/$2/.cmakefiles/repohelper.sh $1"
+
 # # Check if the variables are set
 if [[ "$1" == "init" ]]; then
 echo "init"
-#  cp "/workspaces/cpp/$2/.cmakefiles/templates/* /workspaces/cpp/$2 -R"
+#  cp "$2/$REPO_NAME/.cmakefiles/templates/* /workspaces/cpp/$2 -R"
 
 elif [[ "$1" == "app" ]]; then
 echo "app"
-#  cp "/workspaces/cpp/$2/.cmakefiles/templates/Modules/App1* /workspaces/cpp/$2/Modules/Module1 -R"
+#  cp "$2/$REPO_NAME/.cmakefiles/templates/Modules/App1* /workspaces/cpp/$2/Modules/Module1 -R"
 
 elif [[ "$1" == "module" ]]; then
 echo "module"
-#  cp "/workspaces/cpp/$2/.cmakefiles/templates/Apps/App1* /workspaces/cpp/$2/Apps/App1 -R"
+#  cp "$2/$REPO_NAME/.cmakefiles/templates/Apps/App1* /workspaces/cpp/$2/Apps/App1 -R"
 
 else
   echo "Called task from invalid start directory (you are not in a valid repo)"
 fi
 
 # fi
-
 
