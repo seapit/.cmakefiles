6aaa6f978bf238558fab068777668a8a8a896321
carlo pitruzzello <carlo.pitruzzello+git@gmail.com>
2024-07-14 06:10:15 +0000

    Slowly fixing build sys

--- CMakeTest.cmake
+++ CMakeTest.cmake
@@ -2,3 +2,5 @@ message(STATUS "Building target '" ${TEST_TARGET} "' (Unit-Tests)..")
 include(GoogleTest)
 
 gtest_discover_tests(${TEST_TARGET} EXTRA_ARGS ${GTEST_EXTRA_ARGS})
+
+target_link_libraries(${TEST_TARGET} ${CURRENT_MODULE} gtest gtest_main gtest_gmock ${TEST_TARGET_LIBS})
