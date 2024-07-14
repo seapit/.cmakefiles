3342b0ef24460503fbee392deb6d39f158c2d874
carlo pitruzzello <carlo.pitruzzello+git@gmail.com>
2024-07-14 09:05:43 +0000

    Finishing up scripts

--- repoaddresser.sh
+++ repoaddresser.sh
@@ -3,9 +3,9 @@
 ARG1=$1
 ARG2=$2
 
-printf "\nRepoaddresser.sh arg1: %s, arg2: %s\n" "$1" "$2"
+printf "\nRepoaddresser.sh arg1: %s, arg2: %s, arg3: %s\n" "$1" "$2" "$3"
 
-/workspaces/cpp/$2/.cmakefiles/repohelper.sh $ARG1 $ARG2
+/workspaces/cpp/$2/.cmakefiles/repohelper.sh $1 $2 $3
 
 # fi
 
--- repohelper.sh
+++ repohelper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-printf "\nRepohelper.sh arg1: %s, arg2: %s\n" "$1" "$2"
+printf "\nRepohelper.sh arg1: %s, arg2: %s, arg3: %s\n" "$1" "$2" "$3"
 
 # # Check if the variables are set
 if [[ "$1" == "init" ]]; then
@@ -9,11 +9,11 @@ if [[ "$1" == "init" ]]; then
 
 elif [[ "$1" == "app" ]]; then
   printf "\nrepohelper.app\n"
-  #cp "/workspaces/cpp/$2/.cmakefiles/templates/Modules/App1* /workspaces/cpp/$2/Modules/Module1 -R"
+  #cp "/workspaces/cpp/$2/.cmakefiles/templates/Apps/App1* /workspaces/cpp/$2/Apps/$3 -R"
 
 elif [[ "$1" == "module" ]]; then
   printf "\nrepohelper.module\n"
-  #cp "/workspaces/cpp/$2/.cmakefiles/templates/Apps/App1* /workspaces/cpp/$2/Apps/App1 -R"
+  #cp "/workspaces/cpp/$2/.cmakefiles/templates/Modules/Module1* /workspaces/cpp/$2/Modules/$3 -R"
 
 else
   echo "Called task from invalid start directory (you are not in a valid repo)"
