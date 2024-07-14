6e309c4e9d63a9b8be98f0590d2ed7b538877d20
carlo pitruzzello <carlo.pitruzzello@gmail.com>
2024-07-14 15:00:45 +0000

    Update to cmake helpers

--- repohelper.sh
+++ repohelper.sh
@@ -3,21 +3,24 @@
 # # Check if the variables are set
 if [[ "$1" == "init" ]]; then
   #printf "\nrepohelper.init\n"
-  cp "/workspaces/cpp/$2/.cmakefiles/templates/" "/workspaces/cpp/$2" "-R"
+  #printf "/workspaces/cpp/%s/.cmakefiles/templates/* /workspaces/cpp/%s/ -R\n" $2 $2 
+  #mkdir -p "/workspaces/cpp/$2/apps" && mkdir -p "/workspaces/cpp/$2/libs"
+  cp "/workspaces/cpp/$2/.cmakefiles/templates/apps" "/workspaces/cpp/$2" "-R"
+  cp "/workspaces/cpp/$2/.cmakefiles/templates/libs" "/workspaces/cpp/$2" "-R"
 
 elif [[ "$1" == "app" ]]; then
   #printf "\nrepohelper.app\n"
-  mkdir -p "/workspaces/cpp/$2/Apps/$3"
-  cp "/workspaces/cpp/$2/.cmakefiles/templates/Apps/App1/" "/workspaces/cpp/$2/Apps/$3" "-R"
+  mkdir -p "/workspaces/cpp/$2/apps/$3"
+  cp "/workspaces/cpp/$2/.cmakefiles/templates/apps/App1/" "/workspaces/cpp/$2/apps/$3" "-R"
 
 elif [[ "$1" == "module" ]]; then
   #printf "\nrepohelper.module\n"
-  mkdir -p "/workspaces/cpp/$2/Modules/$3"
-  cp "/workspaces/cpp/$2/.cmakefiles/templates/Modules/Module1/" "/workspaces/cpp/$2/Modules/$3" "-R"
+  mkdir -p "/workspaces/cpp/$2/libs/$3"
+  cp "/workspaces/cpp/$2/.cmakefiles/templates/libs/Module1/" "/workspaces/cpp/$2/libs/$3" "-R"
 
 elif [[ "$1" == "copy" ]]; then
   #printf "\nrepohelper.module\n"
-  cp "/workspaces/cpp/$2/$3" "/workspaces/cpp/$2/Modules/$3" "-R"
+  cp "/workspaces/cpp/$2/$3" "/workspaces/cpp/$2/libs/$3" "-R"
 
 
 else
